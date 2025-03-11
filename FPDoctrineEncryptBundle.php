80ce50d8ee2ce722be3e2b442890a21db83f2f08
Nilan Amarasinghe <ext-nilan.amarasingue@logalty.com>
2025-03-11 11:48:29 +0530

    HOR-7805: fix Symfony 6.4 compatibility issues

--- FPDoctrineEncryptBundle.php
+++ FPDoctrineEncryptBundle.php
@@ -7,16 +7,17 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use FP\DoctrineEncryptBundle\DependencyInjection\FPDoctrineEncryptExtension;
 use FP\DoctrineEncryptBundle\DependencyInjection\Compiler\RegisterServiceCompilerPass;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
-
-class FPDoctrineEncryptBundle extends Bundle {
-    
-    public function build(ContainerBuilder $container) {
+class FPDoctrineEncryptBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
         parent::build($container);
         $container->addCompilerPass(new RegisterServiceCompilerPass(), PassConfig::TYPE_AFTER_REMOVING);
     }
-    
-    public function getContainerExtension()
+
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new FPDoctrineEncryptExtension();
     }
