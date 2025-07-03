ec0139735e7a28b39a63847d7d26a6c70f9a74b9
oriol.bernado <oriol.bernado@logalty.com>
2025-07-03 14:07:38 +0200

    HOR-8120: update symfony 6.4

--- FPDoctrineEncryptBundle.php
+++ FPDoctrineEncryptBundle.php
@@ -5,13 +5,13 @@ namespace FP\DoctrineEncryptBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use FP\DoctrineEncryptBundle\DependencyInjection\FPDoctrineEncryptExtension;
 use FP\DoctrineEncryptBundle\DependencyInjection\Compiler\RegisterServiceCompilerPass;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
-class FPDoctrineEncryptBundle extends Bundle
-{
-    public function build(ContainerBuilder $container)
+class FPDoctrineEncryptBundle extends Bundle {
+
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new RegisterServiceCompilerPass(), PassConfig::TYPE_AFTER_REMOVING);
